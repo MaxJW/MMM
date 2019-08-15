64869e059c49410a6b699e439972a443d6b9302f
Max Wilson <maxjw98@gmail.com>
2019-08-15 17:04:50 +0100

    Started on Spotify
    Got a long way to go, hopefully will get this working by tonight

---
+++ modules/spotify/nowplaying.js
@@ -0,0 +1,22 @@
+
+
+(function ($) {
+    'use strict';
+
+    var apiEndpoint = 'https://api.spotify.com/v1/me/player';
+    getSpotifyData = function() {
+        var blah;
+
+        let options = {
+            url: apiEndpoint,
+            headers: { 'Authorization': 'Bearer ' + this.credentials.accessToken },
+            json: true
+        };
+
+        request.get(options, function(request) {blah.build(request.items)});
+    }
+
+    build = function() {
+        
+    }
+})(jQuery, window, document);
