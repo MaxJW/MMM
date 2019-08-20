7d77a68200b3798438e7468a9dc8c199991aaa33
Max Wilson <maxjw98@gmail.com>
2019-08-20 13:02:32 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 var config = {
-    OWM_KEY : 'OPEN-WEATHER-MAP-KEY-HERE',
-    GCAL_ID : 'GOOGLE-CALENDAR-ID-HERE',
-    GCAL_API_KEY : 'GOOGLE-CALENDAR-API-KEY-HERE',
-    SpotClientID : 'SPOTIFY-CLIENT-ID-HERE',
-    COINMARKET_CAP_KEY : 'COINMARKET-CAP-KEY-HERE',
+    OWM_KEY : 'OPEN-WEATHER-MAP-KEY-HERE', //https://openweathermap.org/api
+    GCAL_ID : 'GOOGLE-CALENDAR-ID-HERE', //https://cloud.google.com
+    GCAL_API_KEY : 'GOOGLE-CALENDAR-API-KEY-HERE', //https://cloud.google.com
+    SpotClientID : 'SPOTIFY-CLIENT-ID-HERE', //https://developer.spotify.com/
+    COINMARKET_CAP_KEY : 'COINMARKET-CAP-KEY-HERE', //https://coinmarketcap.com/api/
 }
