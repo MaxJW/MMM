019709e4dbc22efdf03c7417879edca2f8c5d081
Max Wilson <maxjw98@gmail.com>
2025-11-06 12:16:04 +0000

    Updated update script

--- update.sh
+++ update.sh
@@ -4,9 +4,6 @@
 # Pulls latest changes, installs dependencies, builds, and restarts services
 
 git pull && \
-bun i && \
-bun run build && \
-sudo systemctl daemon-reload && \
-sudo systemctl restart dashboard-webapp && \
+docker compose build && \
+docker compose up -d && \
 sudo systemctl restart dashboard-kiosk
-
